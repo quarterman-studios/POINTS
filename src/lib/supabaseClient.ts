583e74a3b4fcfdc225446893e153c9dc6c3ee281
TTorKori <vickegbu@gmail.com>
2025-09-05 12:28:41 +0100

    Initialised User Authentication Feature

---
+++ src/lib/supabaseClient.ts
@@ -0,0 +1,2 @@
+import { VITE_PUBLIC_SUPABASE_PUBLISHABLE_KEY, VITE_PUBLIC_SUPABASE_URL } from "$env/static/private"
+import { createServerClient } from "@supabase/ssr"
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>POINTS</h1>
+
+<h2>LEADERBOARD</h2>
+
+<form method="POST" action="?/signin">
+	<div>
+		<label for="username">Username: </label>
+		<input name="username" id="username" type="text" />
+	</div>
+	<div>
+		<label for="email">Email: </label>
+		<input name="email" id="email" type="text" />
+	</div>
+	<div>
+		<label for="password">Password: </label>
+		<input name="password" id="password" type="password" required />
+	</div>
+</form>
